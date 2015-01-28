3d26701b209dc09064fd40deb45bdd488f1e0459
edz504 <eddiedzhou@gmail.com>
2015-01-28 15:16:45 -0500

    instagram auth finished

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from flask import request
 from flask import Flask
 from flask import render_template
 from instagram.client import InstagramAPI
@@ -6,15 +7,29 @@ from instagram.client import InstagramAPI
 app = Flask(__name__)
 app.debug = True
 
-# @app.route('/')
-# def landing():
-#     return render_template('')
+# CONFIG redirect change for production
+# (also change on Instagram API)
+
+CONFIG = {
+    'client_id': '15f6735cc27f4e51820653e4ab91bfae',
+    'client_secret': 'a4baf8708e504915a8d2f0bf2f9a3bba',
+    'redirect_uri': 'http://localhost:5000/insights'
+}
+
+unauthenticated_api = InstagramAPI(**CONFIG)
 
 @app.route('/')
+def landing():
+    url = unauthenticated_api.get_authorize_url()
+    return render_template('cover.html',
+        url=url)
+
+@app.route('/insights')
 def eddie():
-    with open('my_token.data', 'r') as f:
-        acc_token = f.read()
-    api = InstagramAPI(access_token=acc_token)
+    code = request.args['code']
+    access_token, user_info = unauthenticated_api.exchange_code_for_access_token(code)
+    # return str(user_info.keys())
+    api = InstagramAPI(access_token=access_token)
     recent_media, next_ = api.user_recent_media()
     likes = [p.like_count for p in recent_media]
     times = [p.created_time for p in recent_media]
@@ -27,14 +42,9 @@ def eddie():
     times.reverse()
 
     return render_template('test_chartjs.html',
-        username='edz504',
+        username=user_info['username'],
         likes=likes,
         times=range(0, len(times)))
-    # return 'Hello'
-
-@app.route('/test')
-def test():
-    return render_template('cover.html')
 
 # below is for development only (when running python hello.py)
 if __name__ == '__main__':
